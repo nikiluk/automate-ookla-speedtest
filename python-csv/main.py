4e1c4b385b5facad6932070114c6c48da550ed3e
Jamie Pond <jamie.pond@relab.dk>
2022-01-26 02:31:59 +0000

    Make output more expressive, fix bug in download speed output and fix warnings

--- python-csv/main.py
+++ python-csv/main.py
@@ -1,29 +1,29 @@
 
-import os
 import subprocess
 import datetime
 import time
 from csv import writer
 
-# Edit the values you want before you run the script here.
-# I've had success with even appending to files on my remote
-# Google Drive (so many computers can feed into the same database!)
-output_csv         = "Y:\speedtest_output.csv"
-interval_seconds   = 2
-hours_to_run_for   = 0.5
-seconds_to_run_for = hours_to_run_for * 60 * 60
-print("Running for", seconds_to_run_for, "seconds...")
 
-
-def append_to_csv(csv_to_append, list):
-    with open(csv_to_append, 'a', newline='') as f_object:
+def append_to_csv(csv_to_append_to, list_to_append):
+    with open(csv_to_append_to, 'a', newline='') as f_object:
         writer_object = writer(f_object)
-        writer_object.writerow(list)
+        writer_object.writerow(list_to_append)
         f_object.close()
 
 
 def get_time_str(num_seconds_elapsed):
     return time.strftime("%Hh%Mm%Ss", time.gmtime(num_seconds_elapsed))
+
+
+# Edit the values you want before you run the script here.
+# I've had success with even appending to files on my remote
+# Google Drive (so many computers can feed into the same database!)
+output_csv         = r"Y:\speedtest_output.csv"
+interval_seconds   = 2
+hours_to_run_for   = 0.5  # half an hour
+seconds_to_run_for = hours_to_run_for * 60 * 60
+print("Running for", get_time_str(seconds_to_run_for))
 
 
 if __name__ == '__main__':
@@ -37,17 +37,10 @@ if __name__ == '__main__':
         output = subprocess.check_output("speedtest --format=csv")
         output_with_time_prepended = str(date_and_time) + "," + str(output.decode("utf-8")).replace('"', "")
         output_list = output_with_time_prepended.split(",")
-        print("Finished speed test with download speed:", str(float(output_list[7]) / 100000.0), "Mbps")
+        print("Finished speed test with download speed:", str(float(output_list[6]) / 1000000.0), "Mbps")
         append_to_csv(output_csv, output_list)
         num_tests += 1
         print("Number of tests completed:", num_tests, "in", get_time_str(time.time() - start_time))
         time.sleep(interval_seconds)
 
     print("Finished testing!")
-
-
-
-
-
-
-
